86dbd0a6cf8cbeb9e016bf06126528bf18872ba2
Tony-viewz <anthonyosuji32@gmail.com>
2023-05-06 15:04:12 +0100

    update

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 // document.getElementById("count").innerText = 5
 
-// let firstBatch = 5
-// let secondBatch = 7
-// let count = firstBatch + secondBatch
-// console.log(count)
-
-// initialize the count as 0
-// listen for clicks on the increment button
-// increase the count variable when the button is clicked
 //change the count-el in the HTML to reflect the new count
 
-let startCount = 0
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
 
  function increment() {
     count = count + 1
-    console.log(count)  
+    countEl.innerText = count 
  }
